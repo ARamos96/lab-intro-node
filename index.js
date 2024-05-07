5dbdb7592cf1176105bccc3bf00c0f66be507651
Arnau Ramos <arnauramos21@gmail.com>
2024-05-07 20:57:56 +0200

    iteration 3

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ class SortedList {
 
   add(item) {
     this.items.push(item);
-    this.items.sort( (a, b) => a-b)
+    this.items.sort((a, b) => a-b)
     this.length = this.items.length;
   }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos]) {
+      return this.items[pos]
+    } else {
+      throw new Error("Out of bounds")
+    }
+  }
 
   max() {}
 
